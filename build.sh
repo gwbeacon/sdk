3df1f5125ec62fd11093a37710f3c9301917ccab
Letian Yi <letian0805@gmail.com>
2019-03-13 01:01:30 +0800

    move go.mod to subdir

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 export GO111MODULE=on
 
-go get ./...
-
 protoc --go_out=plugins=grpc:./ v1/*.proto
--- v1/go.mod
+++ v1/go.mod
@@ -1,4 +1,4 @@
-module github.com/gwbeacon/sdk
+module github.com/gwbeacon/sdk/v1
 
 go 1.12
 
